c847abd5f9068e946a7e3a6522c5f93a685d3b5b
Adrian Messias <adrianmessias62@gmail.com>
2025-01-12 12:33:54 -0300

    feat: add authentication clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,20 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+
+
+const LoginPage = async () => {
+    const  {userId} = await auth ();
+    if (userId) {
+        redirect('/')
+       
+    }
+
+
     return (   
         <div className="grid grid-cols-2 h-full ">
             {/* {esquerda} */}
@@ -20,10 +32,12 @@ const LoginPage = () => {
                 >A Finance AI é uma plataforma de gestão financeira 
                 que utiliza IA para monitorar suas movimentações, 
                 e oferecer insights personalizados, facilitando 
-                o controle do seu orçamento
+                o controle do seu orçamento.
                 </p>
+                <SignInButton>
+                    <Button variant="outline" > <LogInIcon className="mr-2"/> Fazer login ou criar conta. </Button>
+                </SignInButton>
                 
-                <Button variant="outline" > <LogInIcon className="mr-2"/> Fazer login ou criar conta. </Button>
             
             
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
+import { redirect } from "next/navigation";
 
-const Home = () => {
-  return <div><Button>Click me</Button></div>;
+
+const Home =  async () => {
+  const {userId} = await auth();
+  if(!userId){
+    redirect('/login');
+  }
+  return (
+    <div className="h-full flex items-center justify-center">
+        
+        <UserButton 
+        showName
+        appearance={{
+          baseTheme:dark,
+        }}
+         />
+      
+    </div>
+    
+  )
   
 };
 
